1404514fec57bd691df1e00676dd540fdaa57487
mcdevin <mcdevin@sandia.gov>
2024-05-09 20:41:22 -0600

    working on getting functional C++ script

--- cpp/moordyn_cpp_test.cpp
+++ cpp/moordyn_cpp_test.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 #include "Config.h"
 #include "MoorDyn2.hpp"
 
@@ -6,26 +7,29 @@ using namespace moordyn;
 
 int main(int, char**)
 {
+    // define system
     auto system = new MoorDyn("../../MoorDyn/oc3.txt");
     if (!system)
         return 1;
+    
+    // get general system properties
+    auto points = system->GetPoints();
+    int num_points = points.size();
+    auto lines = system->GetLines();
+    int num_lines = lines.size();
 
-    // 3 coupled points x 3 components per point = 9 DoF
-    double x[9], xd[9];
+    // 3 components per point
+    double x[system->NCoupledDOF()], xd[system->NCoupledDOF()];
+
+    // initial velocity is zero
     memset(xd, 0.0, sizeof(double));
-    // Get the initial positions from the system itself
-    for (unsigned int i = 0; i < 3; i++) {
-        // 4 = first fairlead id
-        auto point = MoorDyn::GetPoints(system, i + 4);
-        err = GetPointPos(point, x + 3 * i);
-        if (err != MOORDYN_SUCCESS) {
-            MoorDynClose();
-            return 1;
-        }
-    }
 
+    // get the initial positions
+    for (unsigned int i = 0; i < num_points; i++) {
+
+    }
     // Setup the initial condition
-    int err = MoorDynInit(x, xd);
+    int err = system->Init(x, xd);
     if (err != MOORDYN_SUCCESS) {
         MoorDynClose();
         return 1;
@@ -36,54 +40,31 @@ int main(int, char**)
         xd[3 * i] = 0.5;
     double t = 0.0, dt = 0.5;
     double f[9];
-    err = MoorDyn_Step(system, x, xd, f, &t, &dt);
+    err = system->Step(x, xd, f, t, dt);
     if (err != MOORDYN_SUCCESS) {
-        MoorDyn_Close(system);
+        MoorDynClose();
         return 1;
     }
 
     // Print the position and tension of the line nodes
-    unsigned int n_lines;
-    err = MoorDynGetNumberLines(system, &n_lines);
-    if (err != MOORDYN_SUCCESS) {
-        MoorDyn_Close(system);
-        return 1;
-    }
-    for (unsigned int i = 0; i < n_lines; i++) {
+    
+    for (unsigned int i = 0; i < lines.size(); i++) {
+        Line line = *lines[i];
         const unsigned int line_id = i + 1;
-        printf("Line %u\n", line_id);
-        printf("=======\n");
-        MoorDyn/MoorDynLine line = MoorDyn_GetLine(system, line_id);
-        if (!line) {
-            MoorDynClose(system);
-            return 1;
-        }
-        unsigned int n_nodes;
-        err = MoorDyn_GetLineNumberNodes(line, &n_nodes);
-        if (err != MOORDYN_SUCCESS) {
-            MoorDynClose(system);
-            return 1;
-        }
+        std::cout << "Line " << line.lineId << "\n";
+        std::cout << "=======" << std::endl;
+        unsigned int n_nodes = line.getN() + 1;
         for (unsigned int j = 0; j < n_nodes; j++) {
-            printf("  node %u:\n", j);
-            double pos[3], ten[3];
-            err = MoorDyn_GetLineNodePos(line, j, pos);
-            if (err != MOORDYN_SUCCESS) {
-                MoorDyn_Close(system);
-                return 1;
-            }
-            printf("  pos = [%g, %g, %g]\n", pos[0], pos[1], pos[2]);
-            err = MoorDyn_GetLineNodeTen(line, j, ten);
-            if (err != MOORDYN_SUCCESS) {
-                MoorDyn_Close(system);
-                return 1;
-            }
-            printf("  ten = [%g, %g, %g]\n", ten[0], ten[1], ten[2]);
+            std::cout << "  node " << j << ":" << std::endl;
+            vec pos = line.getNodePos(j);
+            std::cout << "  pos = [" << pos << "]" << std::endl;
+            vec ten = line.getNodeTen(j);
+            std::cout << "  ten = [" << ten << "]" << std::endl;
         }
     }
 
-    // Alright, time to finish!
-    err = MoorDyn_Close(system);
+    // Close program
+    err = MoorDynClose();
     if (err != MOORDYN_SUCCESS)
         return 1;
 
