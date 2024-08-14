314dee9d9fd3797581d57dfeeaa4578e787d293b
mcdevin <mcdevin@sandia.gov>
2024-08-14 17:17:46 -0600

    Simulink crashing when trying to run S-Function

---
+++ MATLAB/MoorDyn_SFunc_test.m
@@ -0,0 +1,9 @@
+% make sure the directory where the MoorDyn_SFunc.mex* file is located
+% is in the MATLAB path
+
+% these variables are defined in the MoorDyn_SFunc block:
+MD_InputFileName = '../MoorDyn/ahm_3dof.txt';
+dt               = 0.01; % seconds
+TMax             = 60; % seconds
+
+sim('../Simulink/AHM_loop.slx',[0,TMax]);
