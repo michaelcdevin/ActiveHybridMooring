4fa5666d9c55cfbfaa599bc2aa3eacda39ed709c
mcdevin <mcdevin@sandia.gov>
2024-08-23 13:29:45 -0600

    reset some Simulink and MD mooring integration time steps

--- MATLAB/MoorDyn_SFunc_test_3dof.m
+++ MATLAB/MoorDyn_SFunc_test_3dof.m
@@ -3,7 +3,7 @@
 
 % these variables are defined in the MoorDyn_SFunc block:
 MD_InputFileName = '../MoorDyn/ahm_3dof.txt';
-dt               = 0.02; % seconds
+dt               = 0.01; % seconds
 TMax             = 60; % seconds
 
 sim('../Simulink/MD_sfunc_test_3dof.slx');
