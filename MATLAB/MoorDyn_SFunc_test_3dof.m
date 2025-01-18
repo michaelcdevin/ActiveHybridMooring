3fc029cc159b41c2c2a2bb15ffca6cd526506360
michaelcdevin <mcdevin@sandia.gov>
2025-01-17 21:39:58 -0700

    completely recreated surrogate model since old one had broken velocities

--- MATLAB/MoorDyn_SFunc_test_3dof.m
+++ MATLAB/MoorDyn_SFunc_test_3dof.m
@@ -2,7 +2,7 @@
 % is in the MATLAB path
 
 % these variables are defined in the MoorDyn_SFunc block:
-MD_InputFileName = '../MoorDyn/ahm_3dof.txt';
+MD_InputFileName = '../MoorDyn/ahm_3dof_fixedmaybe.txt';
 dt               = 0.01; % seconds
 TMax             = 60; % seconds
 
--- SurrogateModel/run_ahm_model.m
+++ SurrogateModel/run_ahm_model.m
@@ -2,8 +2,8 @@ clear
 clc
 
 pyenv('Version', 'C:\Users\mcdevin\AppData\Local\miniforge3\envs\ahm\python.exe');
-MD_InputFileName = '../MoorDyn/ahm_3dof.txt';
+MD_InputFileName = '../MoorDyn/ahm_3dof_fixedmaybe.txt';
 dt               = 0.01; % seconds
-TMax             = 0.1; % seconds
+TMax             = 10; % seconds
 
 sim('ahm_model_no_actuators.slx');
