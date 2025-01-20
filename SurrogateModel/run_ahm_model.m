51e329f2b55a9f9ca722e25ad8b47eef8fa143a2
michaelcdevin <mcdevin@sandia.gov>
2025-01-20 11:35:29 -0700

    switched Simulink model out of debugging mode

--- SurrogateModel/run_ahm_model.m
+++ SurrogateModel/run_ahm_model.m
@@ -2,8 +2,8 @@ clear
 clc
 
 pyenv('Version', 'C:\Users\mcdevin\AppData\Local\miniforge3\envs\ahm\python.exe');
-MD_InputFileName = '../MoorDyn/ahm_3dof_fixedmaybe.txt';
+MD_InputFileName = '../MoorDyn/ahm_3dof.txt';
 dt               = 0.01; % seconds
-TMax             = 10; % seconds
+TMax             = 2; % seconds
 
 sim('ahm_model_no_actuators.slx');
